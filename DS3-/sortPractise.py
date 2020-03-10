c8e4cc11d7f3808a620e4f3ce92a7f0e1da1b30b
Yjingqi <1598655967@qq.com>
2020-03-10 15:44:10 +0800

    排序

---
+++ DS3-/sortPractise.py
@@ -0,0 +1,27 @@
+'''
+
+
+
+'''
+
+
+
+
+def bubbleSort(alist):
+
+    flag = True
+    for passnum range(len(alist)-1,0,1):
+        for i in range(passnum)
+            temp = alist[i]
+            alist[i] = alist[i+1]
+            alist[i+1] = temp
+alist = [54,55,93,55,64,78,12,33]
+
+bubbleSort(alist)
+print(alist)
+
+
+
+
+
+
